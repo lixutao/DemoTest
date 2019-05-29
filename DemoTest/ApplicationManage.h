62af08b91ec2d860a34b3d4a0370966e71a717d8
lixutao <lixt@nuanxin-health.com>
2019-05-29 15:21:43 +0800

    first commit

---
+++ DemoTest/ApplicationManage.h
@@ -0,0 +1,17 @@
+//
+//  ApplicationManage.h
+//  DemoTest
+//
+//  Created by lixutao on 2019/5/29.
+//  Copyright © 2019年 lixutao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ApplicationManage : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DemoTest/ApplicationManage.m
@@ -0,0 +1,13 @@
+//
+//  ApplicationManage.m
+//  DemoTest
+//
+//  Created by lixutao on 2019/5/29.
+//  Copyright © 2019年 lixutao. All rights reserved.
+//
+
+#import "ApplicationManage.h"
+
+@implementation ApplicationManage
+
+@end
